66dae9f015a62bcf4106ccc98d5d22afd8829992
Marc Bernal <mbernal@regosdevstudio.com>
2021-07-06 13:06:26 -0500

    UAT3

--- snakes-and-ladders-tests/TokenCanMoveAcrossTheBoardTests.cs
+++ snakes-and-ladders-tests/TokenCanMoveAcrossTheBoardTests.cs
@@ -22,7 +22,15 @@ namespace snakes_and_ladders_tests
         [Fact]
         public void TokenMoves3Positions() {
             _game.Move(3);
-            Assert.True(_game.CurrentPosition == 4, "The token is not on position 4.");         
+            Assert.True(_game.CurrentPosition == 4, "The token is not at position 4.");         
+        }
+
+        //UAT3
+        [Fact]
+        public void TokenMovesTwoTimesToPosition8() {
+            _game.Move(3);
+            _game.Move(4);
+            Assert.True(_game.CurrentPosition == 8, "The token is not at position 8.");
         }
     }
 }
